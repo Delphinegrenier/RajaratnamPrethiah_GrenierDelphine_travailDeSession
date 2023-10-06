0b25b9caab3d246b2f91efb21b2cca4a906c277e
Delphine <1554307@cmontmorency.qc.ca>
2023-10-05 20:06:25 -0400

    modification des objets et tableaux - ajout de fonction pour gérer dom et affichage des éléments

--- page3.js
+++ page3.js
@@ -5,46 +5,177 @@
   alert(alerteSondage);
 })();
 
-const soumissionBouton = document.getElementById("soumissionBoutonFormulaire");
-soumissionBouton.addEventListener("click", function (event) {
-  event.preventDefault();
-  setTimeout(function () {
-    window.location.href = "page4.html";
-  }, 2000);
-});
-
 // Tableau contenant des questions et des options
-const sondageQuestions = [
-  {
+let sondage = {
+  base: {
     question: "Quelle base préférez-vous pour votre thé?",
     type: "choixsimple",
-    options: ["Thé", "Lait", "Jus"],
+    options: [
+      {
+        choix: "Thé",
+      },
+      {
+        choix: "Lait",
+      },
+      {
+        choix: "Jus",
+      },
+    ],
+    bouton: [
+      {
+        destination: "garniture",
+        titre: "Continuer",
+      },
+    ],
   },
-  {
+  garniture: {
     question: "Quelles garnitures ajoutez-vous à votre boisson?",
     type: "choixmultiple",
     options: [
-      "Perles de tapioca",
-      "Perles éclatantes",
-      "Morceaux de fruits",
-      "Haricots",
-      "Aucune garniture",
+      {
+        choix: "Perles de tapioca",
+      },
+      {
+        choix: "Perles éclatantes",
+      },
+      {
+        choix: "Morceaux de fruits",
+      },
+      {
+        choix: "Haricots",
+      },
+      {
+        choix: "Aucune garniture",
+      },
+    ],
+    bouton: [
+      {
+        destination: "taille",
+        titre: "Continuer",
+      },
     ],
   },
-  {
+  taille: {
     question: "Généralement, quelle taille de boisson commandez-vous?",
     type: "choixsimple",
-    options: ["Petit", "Moyen", "Grand"],
+    options: [
+      {
+        choix: "Petit",
+      },
+      {
+        choix: "Moyen",
+      },
+      {
+        choix: "Grand",
+      },
+    ],
+    bouton: [
+      {
+        destination: "sucre",
+        titre: "Continuer",
+      },
+    ],
   },
-  {
+
+  sucre: {
     question: "Quel niveau de sucre choisissez-vous?",
     type: "choixsimple",
-    options: ["100%", "75%", "50%", "25%", "0%"],
+    options: [
+      {
+        choix: "100%",
+      },
+      {
+        choix: "75%",
+      },
+      {
+        choix: "50%",
+      },
+      {
+        choix: "25%",
+      },
+      {
+        choix: "0%",
+      },
+    ],
+    bouton: [
+      {
+        destination: "frequence",
+        titre: "Continuer",
+      },
+    ],
   },
-  {
+  frequence: {
     question:
       "À quelle fréquence consommez-vous des boissons de type thé aux perles hebdomadairement?",
     type: "inputtexte",
-    options: null,
+    options: [
+      {
+        choix: "Une fois",
+      },
+      {
+        choix: "2-3 fois",
+      },
+      {
+        choix: "4-5 fois",
+      },
+      {
+        choix: "6+",
+      },
+    ],
+    bouton: [
+      {
+        destination: "null",
+        titre: "Envoyer",
+      },
+    ],
   },
-];
+};
+
+function afficherQuestions(cle) {
+  let questions = document.querySelector("h2");
+  questions.innerText = sondage[cle].question;
+  const inputs = document.querySelector(".barreoptions");
+  let containerBoutons = document.querySelector(".containerBoutons");
+
+  // enleve les inputs pour ne pas qu'ils se dupliques
+  while (inputs.firstChild) {
+    inputs.removeChild(inputs.firstChild);
+  }
+
+  // enleve les boutons pour ne pas qu'ils se dupliques
+  while (containerBoutons.firstChild) {
+    containerBoutons.removeChild(containerBoutons.firstChild);
+  }
+  // boucle qui crée des inputs pour chaque options à chaque nouvelle clé
+  for (let i in sondage[cle].options) {
+    const nouveauLabel = document.createElement("label");
+    nouveauLabel.innerText = sondage[cle].options[i].choix;
+    const nouveauInput = document.createElement("input");
+    nouveauInput.setAttribute("type", "radio");
+    nouveauInput.setAttribute("name", "reponse");
+
+    inputs.appendChild(nouveauInput);
+    inputs.appendChild(nouveauLabel);
+  }
+
+  // boucle qui crée un bouton pour chaque clé avec une destination
+  for (let i = 0; i < sondage[cle].bouton.length; i++) {
+    const nouveauBtn = document.createElement("button");
+
+    nouveauBtn.textContent = sondage[cle].bouton[i].titre;
+    containerBoutons.appendChild(nouveauBtn);
+    if (cle === "frequence") {
+      nouveauBtn.addEventListener("click", function (event) {
+        event.preventDefault();
+        setTimeout(function () {
+          window.location.href = "page4.html";
+        }, 2000);
+      });
+    } else {
+      nouveauBtn.addEventListener("click", () => {
+        afficherQuestions(sondage[cle].bouton[i].destination);
+      });
+    }
+  }
+}
+afficherQuestions("base");
