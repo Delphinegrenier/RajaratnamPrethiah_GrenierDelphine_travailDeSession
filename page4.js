78af160a3f7b9cdf7c1b8709d58e1fa59eb6d2a4
rprethi <2271821@cmontmorency.qc.ca>
2023-10-29 18:28:41 -0400

    modif affichage reponse

--- page4.js
+++ page4.js
@@ -1,27 +1,36 @@
 // Importer les objets stockés depuis le stockage local (localStorage)
 // Récupération de l'objet utilisateur préalablement stocké
+
 let monUtiliStrStocker = localStorage.getItem("monUtilisateur");
 let monUtiliStocker = JSON.parse(monUtiliStrStocker);
 
-// Récupération de l'objet sondage préalablement stocké
 let monSondageStrStocker = localStorage.getItem("monSondage");
 let monSondageStocker = JSON.parse(monSondageStrStocker);
 
 // Affichage des objets récupérés dans la console
-console.log(monUtiliStocker);
-console.log(monSondageStocker);
+console.log("Utilisateur:", monUtiliStocker);
+console.log("Sondage:", monSondageStocker);
 
-//Affichage des éléments stocker dans la page
+// Affichage des éléments stockés dans la page
 const salutationH2 = document.querySelector("#salutation");
 const choixUl = document.querySelector("#vosChoix");
-const nomNode = document.createTextNode(monUtiliStocker.ID);
-
-console.log(monSondageStocker.base.reponse);
+const nomNode = document.createTextNode(
+  "Nom de l'utilisateur : " + monUtiliStocker.ID
+);
 salutationH2.appendChild(nomNode);
 
 for (let item in monSondageStocker) {
   const nouvLi = document.createElement("li");
-  const choixNode = document.createTextNode(item);
+  const choixNode = document.createTextNode(
+    item + ": " + monSondageStocker[item].reponse
+  );
   nouvLi.appendChild(choixNode);
   choixUl.appendChild(nouvLi);
 }
+
+//Affichage dans la console les réponses
+console.log(monSondageStocker.base.reponse);
+console.log(monSondageStocker.garniture.reponse);
+console.log(monSondageStocker.taille.reponse);
+console.log(monSondageStocker.sucre.reponse);
+console.log(monSondageStocker.frequence.reponse);
